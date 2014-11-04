3a857cd7e454ad629e2743279b87d438f8968a2b
11696352 <11696352@student.uts.edu.au>
2014-11-05 04:53:35 +1100

    GOTY

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div id="push">
 </div>
 <div id="footer">
-<h2>Spooky UTS Travel Lodge System by noscopes © 2014 Gill</p>
+<h2>Spooky UTS Travel Lodge System by noscopes © 2014 Gill</h2>
 <img src="images/5spooky3me.gif" width="20%"/>
 </div>
 </body>
