c0aebfb73211d951d46da84244eb0173ca0b8c4a
11696352 <11696352@student.uts.edu.au>
2014-11-05 22:11:30 +1100

    swag

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
    <link href="css/chairmember.css" rel="stylesheet" type="text/css"/>
    <script src="js/inputValidate.js"></script>
 <title>UTS Travel Lodging System</title>
-<?php if (1 + 1 == 2) { ?>
+<?php if (isset($_SESSION['user_id'])) { ?>
 <p class="spooky" id="right">You are currently logged in as Mr Noscope<br>
 This is not me?<form action="index.php" method="post">
 <a href="index.php"><input type="button" value="Log out" class="spookybutton" /></a></p>
