b0ec9bda5ffb3bae7c550b13f651f2607de22d44
11696352 <11696352@student.uts.edu.au>
2014-11-06 13:54:25 +1100

    sougs

--- header.php
+++ header.php
@@ -4,6 +4,21 @@
    <link href="css/main.css" rel="stylesheet" type="text/css"/>
    <link href="css/chairmember.css" rel="stylesheet" type="text/css"/>
    <script src="js/inputValidate.js"></script>
+   <script>
+function stop(audioFile)
+  {
+    var audie = document.getElementById("myAudio");
+	var audio = document.getElementById("myAudio2");
+	var audii = document.getElementById("myAudio3");
+	var audiu = document.getElementById("myAudio4");
+	var audik = document.getElementById("myAudio5");
+    audie.pause();
+	audii.pause();
+	audio.pause();
+	audiu.pause();
+	audik.pause();	
+  }
+</script>
 <title>UTS Travel Lodging System</title>
 <?php if (isset($_SESSION['user_id'])) { ?>
 <p class="spooky" id="right">You are currently logged in as Mr Noscope<br>
@@ -11,8 +26,26 @@ This is not me?<form action="index.php" method="post">
 <a href="index.php"><input type="button" value="Log out" class="spookybutton" /></a></p>
 </form>
 <?php } ?>
-<embed loop="true" src="spooky2.mp3" autostart="true" height="1px" width="1px">
-<p>If you are reading this it means you have been spooked by the spooky skeleton.</p>
+<audio id="myAudio"> 
+	<source src="spooky.mp3">	
 </audio>
+<audio id="myAudio2"> 
+<source src="spooky2.mp3">
+</audio>
+<audio id="myAudio3"> 
+	<source src="SongName.mp3">
+</audio>
+<audio id="myAudio4"> 
+	<source src="hype.mp3">
+</audio>
+<audio id="myAudio5"> 
+	<source src="boo.mp3">
+</audio>
+<button onclick="document.getElementById('myAudio').play()">Play spooky song</button>
+<button onclick="document.getElementById('myAudio2').play()">Play rolls</button>
+<button onclick="document.getElementById('myAudio3').play()">Play Song Name?</button>
+<button onclick="document.getElementById('myAudio4').play()">Play HYPE!!</button>
+<button onclick="document.getElementById('myAudio5').play()">Play the best song ever!</button>
+<button onclick="stop()">Stop</button>
 </head>
 <body>
